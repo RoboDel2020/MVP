7d00df193403554483d5dd8c8e8352c598f15c66
artakmeloyan <meloyanartak@yahoo.com>
2022-04-29 16:12:55 -0500

    maps in assign robot page is complete

--- RoboDel/Controllers/AssignRobotController.cs
+++ RoboDel/Controllers/AssignRobotController.cs
@@ -23,6 +23,8 @@ namespace RoboDel.Controllers
         public static string orderLatitude { get; private set; }
         public static string restaurantLongitude { get; private set; }
         public static string restaurantLatitude { get; private set; }
+        public static double distanceFromRestaurantToCustomer { get; private set; }
+        public static double durationFromRestaurantToCustomer { get; private set; }
         private readonly HttpClient _httpClient = new HttpClient();
         public static List<CourierForDelivery> couriersForDelivery { get; private set; }
         public AssignRobotController()
@@ -33,11 +35,14 @@ namespace RoboDel.Controllers
         {
             Database.Init();
             ViewBag.orderID = orderID;
+            ViewBag.distanceFromRestaurantToCustomer = distanceFromRestaurantToCustomer;
+            ViewBag.durationFromRestaurantToCustomer = durationFromRestaurantToCustomer;
             ViewBag.orderLongitude = orderLongitude;
             ViewBag.orderLatitude = orderLatitude;
             ViewBag.restaurantLongitude = restaurantLongitude;
             ViewBag.restaurantLatitude = restaurantLatitude;
-            ViewBag.allActiveCouriersForDelivery = this.GetDistanceAndDuration();
+            ViewBag.allActiveCouriersForDelivery = couriersForDelivery;
+            ViewBag.robots = GetOnlyRobots();
             return View();
         }
 
@@ -63,6 +68,8 @@ namespace RoboDel.Controllers
             orderLatitude = inputOrderLatitude;
             restaurantLongitude = inputRestaurantLongitude;
             restaurantLatitude = inputRestaurantLatitude;
+            this.GetDistanceAndDurationOfRestaurantAndCustomer();
+            this.GetDistanceAndDuration();
             return Json(new { errMsg = "" });
         }
 
@@ -73,20 +80,20 @@ namespace RoboDel.Controllers
             return couriersForDelivery;
         }
 
-        public List<CourierForDelivery> GetDistanceAndDuration()
+        public void GetDistanceAndDuration()
         {
             List<CourierForDelivery> initialCouriers = this.GetAllActiveRobotsWithCurrentStatisticsAndOrders();
-            List<CourierForDelivery> couriersForDelivery = new List<CourierForDelivery>();
+            List<CourierForDelivery> courierssForDelivery = new List<CourierForDelivery>();
             foreach (CourierForDelivery courierForDelivery in initialCouriers)
             {
                 try
                 {
-                    string url = "https://api.openrouteservice.org/v2/directions/foot-walking?api_key=5b3ce3597851110001cf62488303e1e1bcb64deb91105af7032e30d4&start=" + courierForDelivery.Robot.CurrentRobotStatistics.Latitude.ToString() + "," + courierForDelivery.Robot.CurrentRobotStatistics.Longitude.ToString() + "&end=" + restaurantLatitude + "," + restaurantLongitude;
+                    string url = "https://api.openrouteservice.org/v2/directions/foot-walking?api_key=5b3ce3597851110001cf62488303e1e1bcb64deb91105af7032e30d4&start=" + courierForDelivery.Robot.CurrentRobotStatistics.Longitude.ToString() + "," + courierForDelivery.Robot.CurrentRobotStatistics.Latitude.ToString()  + "&end=" + restaurantLongitude + "," + restaurantLatitude;
                     try
                     {
                         if (courierForDelivery.Delivery.Order.Longitude != 0 && courierForDelivery.Delivery.Order.Latitude != 0)
                         {
-                            url = "https://api.openrouteservice.org/v2/directions/foot-walking?api_key=5b3ce3597851110001cf62488303e1e1bcb64deb91105af7032e30d4&start=" + courierForDelivery.Delivery.Order.Latitude.ToString() + "," + courierForDelivery.Delivery.Order.Longitude.ToString() + "&end=" + restaurantLatitude + "," + restaurantLongitude;
+                            url = "https://api.openrouteservice.org/v2/directions/foot-walking?api_key=5b3ce3597851110001cf62488303e1e1bcb64deb91105af7032e30d4&start=" + courierForDelivery.Delivery.Order.Longitude.ToString() + "," + courierForDelivery.Delivery.Order.Latitude.ToString()  + "&end=" + restaurantLongitude + "," + restaurantLatitude ;
                         }
                     }
                     catch
@@ -105,9 +112,36 @@ namespace RoboDel.Controllers
                     courierForDelivery.Duration = 1000;
                 }
                 
-                couriersForDelivery.Add(courierForDelivery);
+                courierssForDelivery.Add(courierForDelivery);
             }
-            return couriersForDelivery;
+            couriersForDelivery = courierssForDelivery;
+        }
+
+        public void GetDistanceAndDurationOfRestaurantAndCustomer()
+        {
+            try
+            {
+                string url = "https://api.openrouteservice.org/v2/directions/foot-walking?api_key=5b3ce3597851110001cf62488303e1e1bcb64deb91105af7032e30d4&start=" + orderLongitude.ToString() + "," + orderLatitude.ToString()  + "&end=" + restaurantLongitude + "," + restaurantLatitude  ;
+                string json = get_web_content(url);
+                dynamic array = JsonConvert.DeserializeObject(json);
+                distanceFromRestaurantToCustomer = array.features[0].properties.summary.distance / 1000;
+                durationFromRestaurantToCustomer = array.features[0].properties.summary.duration / 60;
+            }
+            catch
+            {
+                distanceFromRestaurantToCustomer = 1000;
+                durationFromRestaurantToCustomer = 1000;
+            }
+        }
+
+        public List<Robot> GetOnlyRobots()
+        {
+            List<Robot> robots = new List<Robot>();
+            foreach (CourierForDelivery courierForDelivery in couriersForDelivery)
+            {
+                robots.Add(courierForDelivery.Robot);
+            }
+            return robots;
         }
     }
 }
