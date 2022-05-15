a6ce946c7a039fcc81435010ea316d23157a5834
artakmeloyan <meloyanartak@yahoo.com>
2022-05-15 14:38:05 -0500

    tetelop

---
+++ RoboDel/Controllers/TeleoperateController.cs
@@ -0,0 +1,29 @@
+﻿using RoboDel.Dao;
+using RoboDel.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboDel.Controllers
+{
+    public class TeleoperateController : Controller
+    {
+        public IActionResult Index()
+        {
+            Database.Init();
+
+
+            return View();
+        }
+
+
+
+        public IActionResult GoToTeleoperate()
+        {
+            return Json(new { errMsg = "" });
+        }
+    }
+}
